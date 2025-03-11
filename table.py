5b081771ac6215ca17ff023f54f49669dab9a97c
vashu111ai <vidhusai41414@gmail.com>
2025-03-11 10:40:26 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,60 @@
+def two():
+    a=2
+    while a<=20:
+        print(a)
+        a=a+2
+def three():
+    a=3
+    while a<=30:
+        print(a)
+        a=a+3
+def four():
+    a=4
+    while a<=40:
+        print(a)
+        a=a+4
+def five():
+    a=5
+    while a<=50:
+        print(a)
+        a=a+5
+def six():
+    a=6
+    while a<=60:
+        print(a)
+        a=a+6
+def seven():
+    a=7
+    while a<=70:
+        print(a)
+        a=a+7
+def eight():
+    a=8
+    while a<=80:
+        print(a)
+        a=a+8
+def nine():
+    a=9
+    while a<=90:
+        print(a)
+        a=a+9
+print("this is table program")
+c=input("which table you want from 2 to 10")
+if c== "2":
+    two()
+elif c == "3":
+    three()
+elif c == "4":
+    four()
+elif c == "5":
+    five()
+elif c == "6":
+    six()
+elif c == "7":
+    seven()
+elif c == "8":
+    eight()
+elif c == "9":
+    nine()
+else:
+    print("wrong Input")
